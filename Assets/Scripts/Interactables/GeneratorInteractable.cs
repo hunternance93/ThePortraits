1b302d48dcb31e1726fb65fe74227aa51595ebcf
Hunter Nance <hunternance93@gmail.com>
2022-11-06 13:08:45 -0500

    Setup Generator to work

---
+++ Assets/Scripts/Interactables/GeneratorInteractable.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorInteractable : MonoBehaviour, IInteractable
+{
+    private bool hasBeenRun = false;
+    [SerializeField] private AudioSource soundEffect = null;
+    [SerializeField] private GameObject[] lightsToEnable = null;
+
+    public void Interacted()
+    {
+        if (!hasBeenRun)
+        {
+
+            if (GameManager.instance.Player.InventoryContains("Fuel"))
+            {
+                hasBeenRun = true;
+
+                if (soundEffect != null) soundEffect.Play();
+                GameManager.instance.DisplayMessage("I filled it with fuel.");
+                foreach (GameObject go in lightsToEnable) if (go != null) go.SetActive(true);
+            }
+            else
+            {
+                GameManager.instance.DisplayMessage("I need to find fuel to fill it with.");
+            }
+        }
+        else
+        {
+            GameManager.instance.DisplayMessage("It is already back on, but it doesn't seem to do much.");
+        }
+    }
+}
